48dee16ba5b793e123c7ecb779634c48af011104
Terrance Young <terrance@empowered901.com>
2026-02-24 10:12:34 -0600

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -3,3 +3,8 @@ variable "aws_region" {
   type        = string
   default     = "us-east-2"
 }
+
+# After adding region in providers, variable is needed to make sure:
+  - Region is configurable
+  - Clean separation
+  - Environment-ready
